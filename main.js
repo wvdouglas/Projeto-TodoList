8a8107b1248ee682b80b5a82539e07babe62318a
Douglas Monteiro <do.uglas.14@hotmail.com>
2024-05-15 21:57:56 -0300

    Projeto 90% finalizado!

--- main.js
+++ main.js
@@ -1,79 +1,63 @@
-const sectionTask = document.querySelector('.task');
-const listaTask = document.querySelector('.lista-task');
-const liTask = document.querySelector('.item-lista-task');
-const buttonAdd = document.querySelector('.button-add-task');
-const buttonChecked = document.querySelector('.button-check-task');
-const task = document.querySelector('.task-item');
-const inputTask = document.querySelector('.input-task');
-const arrayTask = [];
-let contador = 0;
+let sectionTask = document.querySelector('.task');
+let listaTask = document.querySelector('.lista-task');
+let liTask = document.querySelector('.item-lista-task');
+let buttonAdd = document.querySelector('.button-add-task');
+let task = document.querySelector('.task-item');
+let inputTask = document.querySelector('.input-task');
+let count = 0;
 
-function Task(){
+function createElementTask(element, params, className){
+    element.appendChild(params);
+    params.classList.add(className)
+    params.setAttribute('id', `${count}`)
+}
 
+function createTask(){
     var listTask = document.createElement('li');
-    listTask.classList.add('item-lista-task');
-    listTask.setAttribute('id', `${contador}`)
-    listaTask.appendChild(listTask);
-
-    var inputCheck = document.createElement('input');
-    inputCheck.classList.add('button-check-task');
-    inputCheck.setAttribute('type' , 'checkbox')
-    listTask.appendChild(inputCheck);
+    createElementTask(listaTask, listTask, 'item-lista-task')
 
     var itemTask = document.createElement('p');
-    itemTask.classList.add('task-item');
-    listTask.appendChild(itemTask);
+    createElementTask(listTask,itemTask, 'task-item')
 
     var createButton = document.createElement('button');
-    createButton.classList.add('button-remove-task');
-    createButton.setAttribute('id', `${contador}`)
-    createButton.onclick = function (event){
+    createElementTask(listTask, createButton, 'button-remove-task')
+    createButton.onclick = (event) => {
         removeTask(event)
     };
-    listTask.appendChild(createButton);
 
     var imageButton = document.createElement('i');
-    imageButton.classList.add('fa-solid') //ganbiarra pois o icone que estava usando contem um nome separado por "espaço" e com isso o JS não aceitar a informação. Por isso a solução foi duplicar a classe adiciona em duas partes. nome original "fa-solid fa-trash"
+    createElementTask(createButton, imageButton, 'fa-solid')
     imageButton.classList.add('fa-trash')
-    imageButton.setAttribute('id', `${contador}`)
-    createButton.appendChild(imageButton);
-
+    //Codigo repetido, pois o icone que estava usando contem um nome separado por "espaço" e com isso o JS não aceitar a informação. Por isso a solução foi duplicar a classe adiciona em duas partes. nome original "fa-solid fa-trash"
     var userTask = inputTask.value;
     itemTask.innerHTML = userTask;
-
 }
 
-function addTask(){
+const buttonAddTask = () => {
     buttonAdd.addEventListener('click', () =>{
-        var taskUser = inputTask.value;
-        contador = contador+1;
-        arrayTask.push(taskUser);
-        Task();
-    })
+        count = count+1;
+        validationTask();
+        }
+    )
 }
 
-
-function removeTask(evento){
+const removeTask  = (evento) => {
     const element = document.getElementById(evento.target.id)
     element.remove();
-    contador--;
-    // console.log(buttonRemove)
-    // buttonRemove.addEventListener('click', (event) => {
-    //     console.log(event.target.id)
-    // })
+    count--;
 }
 
-// function removeTask(){
-//     var valueTask = document.querySelector('.task')
+const validationTask = () => {
+    if(inputTask.value === ''){
+        alert('Digite uma task!!')
+    }
+    else{
+        createTask();
+    }
     
-//     if(valueTask.value =! ''){
-//         buttonRemove.addEventListener('click', () => {
-//             alert('Teste')
-//         })
-//     }
-//    }
+}
 
-addTask();
+buttonAddTask();
 
 
 
